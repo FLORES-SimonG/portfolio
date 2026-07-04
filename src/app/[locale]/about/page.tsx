6bdedd9268a5d56d598730a345695026b6a6efa0
Simón G. Flores <simonflores_10@hotmail.com>
2026-07-04 21:09:21 +0200

    feat: implement AboutPage component with translations and structured content

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,47 +1,5 @@
-import TranslatedHero from "@/components/TranslatedHero";
-import { getTranslations } from "next-intl/server";
+import AboutPage from "@/components/features/about-me/page/about-page";
 
-export default async function AboutPage() {
-  const t = await getTranslations();
-  return (
-    <div className="stack gap-20">
-      <main className="wrapper about">
-        <TranslatedHero titleKey="about.title" />
-
-        <section>
-          <h2 className="section-title">Background</h2>
-          <div className="content">
-            <p>{t("about.firstParagraph")}</p>
-
-            <br />
-            <p>{t("about.secondParagraph")}</p>
-            <br />
-            <p>{t("about.thirdParagraph")}</p>
-            <br />
-
-            <p>{t("about.fourthParagraph")}</p>
-          </div>
-        </section>
-        <section>
-          <h2 className="section-title">Education</h2>
-          <div className="content">
-            <p>Full-Stack Developer - Henry</p>
-            <p>
-              University Technician in Chemistry - Universidad Tecnológica
-              Nacional Villa María
-            </p>
-          </div>
-        </section>
-        <section>
-          <h2 className="section-title">Skills</h2>
-          <div className="content">
-            <p>
-              React | Angular | NextJS | Typescript | JavaScript | Figma |
-              Leadership | Tailwind | Astro | PostgreSQL | NodeJS
-            </p>
-          </div>
-        </section>
-      </main>
-    </div>
-  );
+export default function Page() {
+  return <AboutPage />;
 }
---
+++ src/components/features/about-me/page/about-page.tsx
@@ -0,0 +1,44 @@
+import TranslatedHero from "@/components/TranslatedHero";
+import { getTranslations } from "next-intl/server";
+
+export default async function AboutPage() {
+  const t = await getTranslations("about");
+  return (
+    <div className="stack gap-20">
+      <main className="wrapper about">
+        <TranslatedHero titleKey="about.title" />
+        <section>
+          <h2 className="section-title">Background</h2>
+          <div className="content">
+            <p>{t("firstParagraph")}</p>
+            <br />
+            <p>{t("secondParagraph")}</p>
+            <br />
+            <p>{t("thirdParagraph")}</p>
+            <br />
+            <p>{t("fourthParagraph")}</p>
+          </div>
+        </section>
+        <section>
+          <h2 className="section-title">Education</h2>
+          <div className="content">
+            <p>Full-Stack Developer - Henry</p>
+            <p>
+              University Technician in Chemistry - Universidad Tecnológica
+              Nacional Villa María
+            </p>
+          </div>
+        </section>
+        <section>
+          <h2 className="section-title">Skills</h2>
+          <div className="content">
+            <p>
+              React | Angular | NextJS | Typescript | JavaScript | Figma |
+              Leadership | Tailwind | Astro | PostgreSQL | NodeJS
+            </p>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
